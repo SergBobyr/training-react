59f75aec924d19525122ccbf47fc715eedef5eda
SergBobyr <serg_bobyr@ukr.net>
2023-01-29 15:15:04 +0200

    hw-2-2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import {useEffect, useState} from "react";
 
 const App=() => {
+    const [ship, setShip]=useState([])
+    useEffect(()=>{fetch('https://api.spacexdata.com/v3/launches/').then(value => value.json()).then(value=> setShip(value))},[])
   return (
-    <div >
-     Hello
+      <div >
+          {ship.filter(item=>item.launch_year!=='2020').map( item =>{ return(
+              <div key={item.flight_number}>
+                  {item.flight_number} - {item.mission_name} - {item.launch_year}
+              </div>)})}
     </div>
   );
 }
